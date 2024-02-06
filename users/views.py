4dbf66f66223ce78c7ddbc2781ea8a0b291cd6fe
Ivan Berestianyi <berestvania@gmail.com>
2024-02-06 21:29:08 +0200

    made authorization controller in users/views.py

--- users/views.py
+++ users/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import auth
+
+from users.forms import UserLoginForm
 
 
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('products:home'))
+    else:
+        form = UserLoginForm()
+
+    context = {'form': 'form'}
+    return render(request, 'users/login.html', context=context)
 
 
 def registration(request):
