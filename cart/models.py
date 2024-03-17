8e4b0fbcd74072c10c49c2c103dcc263b3a30766
Ivan Berestianyi <berestvania@gmail.com>
2024-03-17 15:21:26 +0200

    made 2 models for cart: Cart and CartItem

---
+++ cart/models.py
@@ -0,0 +1,22 @@
+from users.models import User
+from products.models import Product
+from django.db import models
+
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def total_price(self):
+        return self.product.price * self.quantity
+
+
+class Cart(models.Model):
+    products = models.ManyToManyField(CartItem)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def total_sum(self):
+        return sum(product.product_sum for product in self.products.all())
+
+
+
