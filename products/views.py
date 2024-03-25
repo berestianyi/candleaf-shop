8047dab19a0e05cab58058f384fb5079b9cb870c
Ivan Berestianyi <berestvania@gmail.com>
2024-03-25 20:55:53 +0200

    added category sorting

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
 from .models import Product, Category
+from cart.models import Cart
 
 
 def index(request):
@@ -8,8 +9,7 @@ def index(request):
         'mainpage_products': products_data[:8],
         'all_products': products_data,
         'popular_products': products_data[:4],
-        'my_range': range(5),
-
+        'my_range': range(5)
     }
     return render(request, 'products/index.html', context=context)
 
@@ -29,18 +29,24 @@ def detail(request, detail_slug):
 
 def discovery(request):
     products_data = Product.objects.all()
+    category = Category.objects.all()
     context = {
         'all_products': products_data,
+        'category': category
     }
     return render(request, 'products/discovery.html', context=context)
 
 
 def sort_by_category(request, category_slug):
     category_info = get_object_or_404(Category, slug=category_slug)
+    products = Product.objects.filter(category=category_info)
+    category = Category.objects.all()
     context = {
-        'category': category_info,
+        'category_info': category_info,
+        'category': category,
+        'products': products
     }
-    return render(request, 'products/')
+    return render(request, 'products/category.html', context)
 
 
 def test(request):
